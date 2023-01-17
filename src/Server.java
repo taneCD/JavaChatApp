d06b5a87de8e8d950d656e7d170b4298140257cc
Nebojsa Tanaskovic <tn.nebojsa@gmail.com>
2023-01-17 18:34:04 +0100

    first commit

---
+++ src/Server.java
@@ -0,0 +1,125 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static jdk.internal.misc.VM.shutdown;
+
+public class Server implements Runnable{
+
+    private ArrayList<ConnectionHandler>connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService pool;
+
+    public Server(){
+        connections=new ArrayList<>();
+        done=false;
+    }
+    @Override
+    public void run(){
+        try {
+            server = new ServerSocket(9999);
+            pool= Executors.newCachedThreadPool();
+            while(!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }
+        } catch (Exception e) {
+            shutdown();
+        }
+
+    }
+    class ConnectionHandler implements Runnable {
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public void brodcast(String message) {
+            for (ConnectionHandler ch : connections) {
+                ch.sendMessage(message);
+            }
+        }
+
+        public void shutdown() {
+            try {
+                done = true;
+                if (!server.isClosed()) {
+                    server.close();
+                }
+                for (ConnectionHandler ch : connections) {
+                    ch.shutdown();
+                }
+            } catch (IOException e) {
+                //ignore
+            }
+        }
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+        //        Colors
+        public static final String ANSI_purple=	"\u001B[35m";
+
+        public static final String ANSI_reset = "\u001B[0m";
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected!");
+                brodcast(nickname + " joined the chat!");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/nick")) {
+                        String[] messageSplit = message.split(" ", 2);
+                        if (messageSplit.length == 2) {
+                            brodcast(nickname + " renamed themselves to " + messageSplit[1]);
+                            System.out.println(nickname + " renamed themselves to " + messageSplit[1]);
+                            nickname = messageSplit[1];
+                            out.println("Successfully changed nickname to " + nickname);
+                        } else {
+                            out.println("No nickname provided.");
+                        }
+                    } else if (message.startsWith("/quit")) {
+                        brodcast(nickname + " left the chat!");
+                        shutdown();
+                    } else {
+                        brodcast(ANSI_purple+nickname + ": " + message+ANSI_reset);
+                    }
+                }
+            } catch (IOException e) {
+                shutdown();
+            }
+
+        }
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+
+        public void shutDown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                //ignore
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
+}
